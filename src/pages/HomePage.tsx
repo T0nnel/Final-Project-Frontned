7a1e920ca672a33b70da70882723589704328edd
Tonnel <154825284+T0nnel@users.noreply.github.com>
2024-07-22 20:20:54 +0300

    First Commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import"./homepage.css"
+
+export const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, impedit. Cum repudiandae nihil non eum perferendis. Suscipit aliquam quas sunt repellat, ipsum hic laudantium eveniet animi cum commodi rem nisi!</h1>
+    </div>
+  );
+}
+
+
---
+++ src/pages/ProductPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ProductPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Product Page</h1>
+    </div>
+  );
+}
+
